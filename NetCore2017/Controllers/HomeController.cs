2f73e85dc4e95c8bafc5bc6a7946dbc51b3cc5a8
Peevee Jace G. Pascual <peevee12@yahoo.com>
2017-04-14 10:39:23 -0600

    Test push command

--- NetCore2017/Controllers/HomeController.cs
+++ NetCore2017/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore2017.Controllers
 {
+    //My first comment
     public class HomeController : Controller
     {
         public IActionResult Index()
